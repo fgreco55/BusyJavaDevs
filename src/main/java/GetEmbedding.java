c67cae545ee08aed3b5a27029f411d641b5efcfe
Frank Greco <fgreco@javasig.com>
2025-10-19 21:29:37 -0400

    Initial commit for example files for the Pearson/OReilly GenAI for Busy Java Developers course.

---
+++ src/main/java/GetEmbedding.java
@@ -0,0 +1,23 @@
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
+import dev.langchain4j.model.output.Response;
+
+import static dev.langchain4j.model.openai.OpenAiEmbeddingModelName.TEXT_EMBEDDING_3_SMALL;
+
+public class GetEmbedding {
+
+    public static void main(String[] argv) {
+        String apiKey = System.getenv("OPENAI_API_KEY");
+
+        EmbeddingModel model = OpenAiEmbeddingModel.builder()
+                .apiKey(apiKey)
+                .modelName(TEXT_EMBEDDING_3_SMALL)
+                .build();
+
+        Response<Embedding> response = model.embed("Imagination is more important than knowledge.");
+        Embedding embedding = response.content();
+
+        System.out.println(embedding);
+    }
+}
---
+++ src/main/java/SimpleChatCompletion.java
@@ -0,0 +1,37 @@
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
+import dev.langchain4j.model.output.Response;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleChatCompletion {
+    public static void main(String[] args){
+
+        ChatModel cmodel = OpenAiChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(OpenAiChatModelName.GPT_4_O)
+                .temperature(0.3)               // docs say higher is more 'creative'.  Set this to 2.0 (max) as an experiment in "creativity"
+                .timeout(Duration.ofSeconds(30))
+                .maxTokens(1024)
+                .build();
+
+        List<ChatMessage> messages = new ArrayList<>();
+
+        SystemMessage sysmsg = new SystemMessage("Respond as a very funny comedian.");
+        UserMessage usermsg = new UserMessage("How should I learn Java?");
+
+        messages.add(sysmsg);	messages.add(usermsg);
+
+        ChatResponse answer = cmodel.chat(messages);
+
+        System.out.println(answer.aiMessage().text());
+    }
+}
